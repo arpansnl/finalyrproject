9fd5abbbf7aae82c48d40322639fa2b646acc7ee
arpansnl <60974565+arpansnl@users.noreply.github.com>
2024-02-27 16:46:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { hostname } = require('os');
 const port=process.env.PORT || 5000;
 
 app.listen(process.env.PORT || 5000, ()=>{
-    console.log(`listening on ${process.env.PORT || 3000}`);
+    console.log(`listening on ${process.env.PORT || 5000}`);
 });
 app.get('/',async(req,res)=>{
     res.send("<h1>hello World!</h1>")
