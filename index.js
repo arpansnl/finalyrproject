7209119ec68a3e65e5e0518f849c8451c584f920
arpansnl <60974565+arpansnl@users.noreply.github.com>
2024-03-02 21:59:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,67 +31,50 @@ const sensors=pkg6;
 //const server1=require('./server.js');
 const { Timestamp } = pkg3;
 pk10.config();
-// var MongoClient = require('mongodb').MongoClient;
-// var url = "mongodb://127.0.0.1:27017";
 
-//app = express().use(express.static(__dirname + '/')),
-//http = require('http').Server(app),
-//io = require('socket.io')(http);
-//app.listen(8000);
-//app.get('/', function(req, res){
-//    res.sendFile(__dirname + '/index.html');
-//});
-
-// io.on('connection', function(socket){
-//     console.log('a user connected');
-// });
-// app.use(express.json());
-// app.post("/", (req,res)=>{
-//     console.log(req);
-//     });
 import pkg from "body-parser";
-// import { readFileSync } from "fs";
-// import path from 'path';
-// import { fileURLToPath } from 'url';
-// import { error } from 'console';
+import { readFileSync } from "fs";
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { error } from 'console';
 
 var optionsget = {
     host : "finalyearproject-5iva.publicvm.com",
-    port: process.env.PORT || 80,
-    //path : '/', // the rest of the url with parameters if needed
+    port: process.env.PORT || 2000,
+    path : '/', // the rest of the url with parameters if needed
     method : 'GET',
-    // key: readFileSync("./key.pem"),
-    // cert: readFileSync("./cert.pem"),
+    key: readFileSync("./key.pem"),
+     cert: readFileSync("./cert.pem"),
     
 }
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-// const { urlencoded, json } = pkg;
-//const server = express();
-// var options = {
-//     key: readFileSync("./key.pem"),
-//     cert: readFileSync("./cert.pem"),
-//     MONGO_CONNECT_URI:"mongodb+srv://arpansnl01:Arpansnl@cluster0.oibycgi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-//  };
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const { urlencoded, json } = pkg;
+
+var options = {
+    key: readFileSync("./key.pem"),
+    cert: readFileSync("./cert.pem"),
+    MONGO_CONNECT_URI:"mongodb+srv://arpansnl01:Arpansnl@cluster0.oibycgi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+ };
 
 
 const httpServer=https.createServer(server);
-httpServer.listen(80,"finalyearproject-5iva.onrender.com",(req,res)=>{
+httpServer.listen(2000,"0.0.0.0",(req,res)=>{
     console.log("2");
-//console.log("server created on"+err);
+console.log("server created on 2000");
 });
 //server.use(urlencoded({ extended: false }));
 httpServer.urlencoded;
 httpServer.json;
 
-// server.listen()
-// var reqGet=httpServer.listen(optionsget, function(req,res)
-// {
-//     httpServer.on(error,(e)=>{console.log(e)});
-//     //console.log('status code:'+res.);
-// })
+server.listen()
+var reqGet=httpServer.listen(optionsget, function(req,res)
+{
+    httpServer.on(error,(e)=>{console.log(e)});
+    //console.log('status code:'+res.);
+})
 
-//server.post("/send",postDados);
+server.post("/send",postDados);
 
 
 server.get("/", function (req, res) {
@@ -104,54 +87,53 @@ var dados = [];
 var info;
 //const { Parser }= require ("simple-text-parser");
  
-// const parser = new Parser();
-// server.use(parser);
-// const x=require('simple-text-parser')
-// server.use(x.new Parser());
-// console.log("1");
-// function GetDados(req, resp) {
-//     resp.setHeader("Access-Control-Allow-Origin", "*");
-//     resp.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-Width, Content-Type, Accept');
-//     resp.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+//const parser = new Parser();
+//server.use(parser);
+
+console.log("1");
+function GetDados(req, resp) {
+    resp.setHeader("Access-Control-Allow-Origin", "*");
+    resp.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-Width, Content-Type, Accept');
+    resp.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
     
-//     resp.send(dados);
-// };
-// function postDados(req,resp){
-// console.log(2);
-//     if (req.query.admin == SECRET_KEY) {
-//         info = {"mq2": req.query.mq2, "mq7": req.query.mq7,"mq135": req.query.mq135,"dust2": req.query.dust2 };
-//         col.insertOne(info);
-//         resp.send({ "Status": 200 });
-//         console.log(info);
-//     }
-//        else{
-//         console.log("error");
+    resp.send(dados);
+};
+function postDados(req,resp){
+console.log(2);
+    if (req.query.admin == SECRET_KEY) {
+        info = {"mq2": req.query.mq2, "mq7": req.query.mq7,"mq135": req.query.mq135,"dust2": req.query.dust2 };
+        col.insertOne(info);
+        resp.send({ "Status": 200 });
+        console.log(info);
+    }
+       else{
+        console.log("error");
         
-//         // .then((db,err)=>{
-//         //  console.log("1");
-//         //     if (err) throw err;
-//         // var dbo = db.db("gasSensor");
-//         // dbo.collection("gas sensors").insertOne(info, function(err, res) {
-//         //     if (err) throw err;
-//         //     console.log("1 document inserted");
-//         //     db.close();
-//         resp.status(401);
-//         resp.send({ "Error": "Admin incorrect" });
-//        }
-//     }
+        // .then((db,err)=>{
+        //  console.log("1");
+        //     if (err) throw err;
+        // var dbo = db.db("gasSensor");
+        // dbo.collection("gas sensors").insertOne(info, function(err, res) {
+        //     if (err) throw err;
+        //     console.log("1 document inserted");
+        //     db.close();
+        resp.status(401);
+        resp.send({ "Error": "Admin incorrect" });
+       }
+    }
     
 
-// server.post('/send',postDados);
+server.post('/send',postDados);
     
-//     //req.setHeader('content-type','text/plain');
-//     // req.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-Width, Content-Type, Accept');
-//     // req.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+    //req.setHeader('content-type','text/plain');
+    // req.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-Width, Content-Type, Accept');
+    // req.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
 
 
-// function DeleteDados(req, resp) {
-//     dados = [];
-//     resp.send("");
-// };
+function DeleteDados(req, resp) {
+    dados = [];
+    resp.send("");
+};
 
 
 
