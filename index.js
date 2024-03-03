266a4e444268d4c9a69acd06384b806ae69ec666
arpansnl <60974565+arpansnl@users.noreply.github.com>
2024-03-03 18:44:27 +0530

    commit

--- index.js
+++ index.js
@@ -3,27 +3,24 @@ import pk10 from 'dotenv';
 import express from 'express';
 import pkg4 from 'https';
 import pkg3 from 'mongodb';
+import pkg13 from 'tls';
 import pkg5 from './connectMongo.js';
 import pkg6 from './mongo.js';
+const tls=pkg13;
 const dns=pkg11;  
 const SECRET_KEY = "Arpan";
 const x=pkg.con;
-//const hostname = '<YOUR_IP>';
 const { MongoClient } = pkg3;
 const https=pkg4;
 const connectDB=pkg5;
-//const connectDB = require("./connectMongo");
-const server=express();
-const client = new MongoClient(process.env.MONGO_CONNECT_URI);
+const mongoclient = new MongoClient(process.env.MONGO_CONNECT_URI);
 // const { response } = require('express');
 // const { type } = require('server/reply');
-const db = client.db("test");
-const col = db.collection("gas sensors");
-//var app = express().use(express.static(__dirname + '/'));
-//var http = require('http').Server();
-//var request = require('request');
-//const { json } = require("http-client");
-//const { hostname } = require('os');
+const db = mongoclient.db("test");
+const collect = db.collection("gas sensors");
+
+
+
 // app.use(express.json());
 // app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(bodyParser.json());
@@ -35,35 +32,21 @@ const { Timestamp } = pkg3;
 pk10.config();
 // dns.lookup("finalyearproject-5iva.onrender.com",console.log);
 import pkg from "body-parser";
-import { error } from 'console';
 import { readFileSync } from "fs";
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { request } from 'http';
-import { Http2ServerRequest } from 'http2';
 
-var optionsget = {
-    host : "0.0.0.0",
-    port: process.env.PORT || 3001,
-    path : '/',
-    method : 'GET',
-    key: readFileSync("./key.pem"),
-     cert: readFileSync("./cert.pem"),
-    
-}
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const { urlencoded, json } = pkg;
+
+
+
 
 var options = {
     key: readFileSync("./key.pem"),
     cert: readFileSync("./cert.pem"),
     MONGO_CONNECT_URI:"mongodb+srv://arpansnl01:Arpansnl@cluster0.oibycgi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
  };
- const httpServer=https.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('okay');
- });
+//  const httpServer=https.createServer((req, res) => {
+//     res.writeHead(200, { 'Content-Type': 'text/plain' });
+//     res.end('okay');
+//  });
 
 
 //httpServer.listen(process.env.PORT,"finalyearproject.onrender.com",console.log("yes"));
@@ -73,41 +56,42 @@ var options = {
 //  console.log("server created on 3001");
 //  });
 //server.use(urlencoded({ extended: false }));
-httpServer.urlencoded;
-httpServer.json;
+// httpServer.urlencoded;
+// httpServer.json;
 
-httpServer.listen(3001,"0.0.0.0");
-var reqGet=httpServer.listen(optionsget, function(req,res)
-{
-    httpServer.on(Http2ServerRequest,(res)=>{console.log(res)});
-    //console.log('status code:'+res.);
-})
+// httpServer.listen(3001,"https://finalyrproject-r935yghii-arpansnls-projects.vercel.app");
+// var reqPost=httpServer.listen(optionsget, function(req,res)
+// {
+//     httpServer.on(Http2ServerRequest,(res)=>{console.log(res);
+//         res.sendFile("index.html",{root: __dirname})});
+//     //console.log('status code:'+res.);
+// })
 
-server.post("/send",postDados);
+// //server.post("/send",postDados);
 
 
-server.get("/", function (req, res) {
-   res.sendFile("index.html",{root: __dirname})
-});
+// // httpServer.listen("/", function (req, res) {
+// //    res.sendFile("index.html",{root: __dirname})
+// // });
 
-server.use('/', express.static('index'));
-server.set("view engine", "ejs");
-var dados = [];
-var info;
-//const { Parser }= require ("simple-text-parser");
+// server.use('/', express.static('index'));
+// server.set("view engine", "ejs");
+// var dados = [];
+// var info;
+// //const { Parser }= require ("simple-text-parser");
  
-//const parser = new Parser();
-//server.use(parser);
+// //const parser = new Parser();
+// //server.use(parser);
 
-console.log("1");
-function GetDados(req, resp) {
-    resp.setHeader("Access-Control-Allow-Origin", "*");
-    resp.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-Width, Content-Type, Accept');
-    resp.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+// console.log("1");
+// function GetDados(req, resp) {
+//     resp.setHeader("Access-Control-Allow-Origin", "*");
+//     resp.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-Width, Content-Type, Accept');
+//     resp.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
     
-    resp.send(dados);
-};
-function postDados(req,resp){
+//     resp.send(dados);
+// };
+function postDados(req){
 console.log(2);
     if (req.query.admin == SECRET_KEY) {
         info = {"mq2": req.query.mq2, "mq7": req.query.mq7,"mq135": req.query.mq135,"dust2": req.query.dust2 };
@@ -131,21 +115,74 @@ console.log(2);
     }
     
 
-server.post('/send',postDados);
+// server.post('/send',postDados);
     
-    //req.setHeader('content-type','text/plain');
-    // req.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-Width, Content-Type, Accept');
-    // req.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+//     //req.setHeader('content-type','text/plain');
+//     // req.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-Width, Content-Type, Accept');
+//     // req.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
 
 
-function DeleteDados(req, resp) {
-    dados = [];
-    resp.send("");
-};
-server.listen(3001,"127.0.0.1");
-
-
-
+// function DeleteDados(req, resp) {
+//     dados = [];
+//     resp.send("");
+// };
+// server.listen(3002,"120.0.0.7",options);
 
 
 
+const server = https.createServer(options,(req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  console.log('Hello World');
+  res.end();
+});
+server.listen('3001',)
+const optionsget = {
+    hostname: '',
+    port: process.env.PORT,
+    path: '/',
+    method: 'GET',
+    Protocol  : 'TLSv1.3',
+    Ciphers: 'ALL',
+    minVersion: "TLSv1.3",
+     Cert: readFileSync("./clientcert.pem"),
+     Key: readFileSync("./clientcert.key")
+  };
+  const optionspost={
+    hostname: '0.0.0.0',
+    port: process.env.PORT,
+    path: '/',
+    method: 'POST',
+    Protocol  : 'TLSv1.3',
+    Ciphers: 'ALL',
+    minVersion: "TLSv1.3",
+     Cert: readFileSync("./clientcert.pem"),
+     Key: readFileSync("./clientcert.key"),
+  };
+//server.listen(3001, '', serverfunction());
+
+ function serverfunction(){
+    const address=server.address;
+  console.log(`Server listening on ${address.address}:${address.port}`);
+  console.log( tls.DEFAULT_MIN_VERSION);
+  server.listen()
+ }
+  const client= express();
+//    client.listen(80,"",clientfunction);
+//    function clientfunction(){
+//   const clientReq = https.request(optionsget, (clientRes) => {
+//     let data = clientRes.data;
+//     clientRes.on('data', postDados(data));
+    
+
+//     clientRes.on('end', () => {
+//       console.log('Response from server:', data);
+//     });
+//   });
+
+//   clientReq.on('error', (error) => {
+//     console.error('Error sending request:', error);
+//   });
+
+//   clientReq.end();
+
+//    }
